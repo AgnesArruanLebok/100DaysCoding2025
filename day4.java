af0b0d042db9259fe59261f650e4fd5f4c8840a2
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-11 16:39:16 +0800

    day4.java

---
+++ day4.java
@@ -0,0 +1,15 @@
+public class day4 {
+        public static void main(String[] args) {
+            // Menggunakan \n untuk baris baru
+            System.out.print("Baris pertama.\nBaris kedua.\n\n");
+
+            // Menggunakan \t untuk tabulasi
+            System.out.println("Nama\t: Agnes");
+            System.out.println("Kelas\t: INF-A\n");
+
+            // Menggunakan \" dan \\
+            System.out.println("Dia berkata, \"Belajar Java itu seru!\"");
+            System.out.println("Lokasi file: C:\\Users\\Public");
+        }
+    }
+
