844cde82eec71a99e70534227892a5d36321e4ee
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-12 18:05:40 +0800

    day5.java

---
+++ day5.java
@@ -0,0 +1,21 @@
+public class day5 {
+    public static void main(String[] args) {
+        //1.byte(8-bit)
+        byte jumlahkursi = 40;
+
+        //2.short (16-bit)
+        short hargabuku = 25000;
+
+        //3.int(32-bit)
+        int jumlahmahasiswa = 1350000;
+
+        //4.long(64-bit)
+        long totalPengunjung = 5000000000L;
+
+        System.out.println(jumlahkursi);
+        System.out.println(hargabuku);
+        System.out.println(jumlahmahasiswa);
+        System.out.println(totalPengunjung);
+
+    }
+}
