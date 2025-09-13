976390a5e6ba8e2d9218f2c33268cfa2a61a40c3
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-13 16:52:28 +0800

    day6.java

---
+++ day6.java
@@ -0,0 +1,8 @@
+public class day6 {
+    public static void main(String[] args) {
+        float tinggibadan = 160.1f;
+        double beratbadan = 50.9;
+        System.out.println("Tinggi badan: "+tinggibadan+ "cm");
+        System.out.println("Berat badan: "+beratbadan + "kg");
+    }
+}
