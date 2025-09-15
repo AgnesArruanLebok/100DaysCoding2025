fc3729e8ca85b314bb8238c0496ba1c8565599a3
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-15 19:50:19 +0800

    day8.java

---
+++ day8.java
@@ -0,0 +1,19 @@
+public class day8 {
+    public static void main(String[] args) {
+        // 1.Alasan masuk ISC
+        String alasanISC = "Alasan saya masuk ISC adalah untuk belajar lebih giat agar mudah lolos di mata kuliah yang sulit.";
+        //2.Alasan memilih prodi informatika
+        String alasanprodi = "Saya memilih prodi Informatika karena prospek kerja di bidang teknologi sangat luas dan menjanjikan untuk masa depan.";
+        //3.merasa salah jurusan atau tidak
+        String pilihanjurusan = "Saya sempat merasa sedikit salah jurusan karena tugas-tugas yang sulit, tapi sejauh ini saya sudah mulai bisa beradaptasi dengan baik.";
+        //4.kendala di prodi informatika
+        String kendala = "Kendala saya di Prodi Informatika, khususnya pada mata kuliah PBO dan DDP, adalah lumayan sulit dalam membaca dan memahami soal," +
+                         " sehingga kadang butuh waktu lebih lama untuk mengerjakannya. Selain itu, saya juga masih kurang latihan ngoding";
+
+
+        System.out.println("1.Alasan masuk isc: "+alasanISC);
+        System.out.println("2.Alasan memilih prodi informatika: "+alasanprodi);
+        System.out.println("3.Apakah salah jurusan: "+pilihanjurusan);
+        System.out.println("4.kendala di prodi informatika: "+kendala);
+    }
+}
