0caa6699267f52fc3f85129450950c513b9f2b9d
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-16 17:39:25 +0800

    day9.java

---
+++ day9.java
@@ -0,0 +1,31 @@
+public class day9 {
+    public static void main(String[] args) {
+        //deklarasi
+        String nama ;
+        String nim;
+        char kelas;
+        int umur;
+
+        //inisialisai
+        nama = "AGNES";
+        nim = "D0224304";
+        kelas= 'A';
+        umur = 19;
+        System.out.println("===Data profil awal===");
+        System.out.println("Nama: "+nama);
+        System.out.println("Nim: "+nim);
+        System.out.println("Kelas: "+kelas);
+        System.out.println("Umur: "+umur);
+
+        //update
+        nama = "ARRUAN LEBOK";
+        nim = " D0224300";
+        kelas = 'D';
+        umur =20;
+        System.out.println("===Data setelah di update===");
+        System.out.println("Nama: "+nama);
+        System.out.println("Nim: "+nim);
+        System.out.println("Kelas: "+kelas);
+        System.out.println("Umur: "+umur);
+    }
+}
