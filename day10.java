f67811d0aa7484bdf48ba794e1eee22d00f637b9
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-17 21:19:53 +0800

    day10.java

--- day10.java
+++ day10.java
@@ -1,11 +1,11 @@
 public class day10 {
     public static void main(String[] args) {
         // segitiga selalu memiliki 3 sisi tetap(konstanta)
-        final int sisi_segitiga = 3;
+        final int SISI_SEGITIGA = 3;
         int jumlahsegitiga = 5;
-        int totalsisi = jumlahsegitiga*sisi_segitiga;
+        int totalsisi = jumlahsegitiga*SISI_SEGITIGA;
 
-        System.out.println("Jumlah sisi pada 1 segitiga: "+sisi_segitiga);
+        System.out.println("Jumlah sisi pada 1 segitiga: "+SISI_SEGITIGA);
         System.out.println("Jumlah sisi dari 5 segitiga: " +totalsisi);
     }
 }
