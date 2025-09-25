e8ddb9672c35cbba5c169c0a44ec84f9fd9b5503
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-25 19:33:56 +0800

    day18.java

---
+++ day18.java
@@ -0,0 +1,9 @@
+public class day18 {
+    public static void main(String[] args) {
+        int hargabarang = 150000;
+        double pajak = 16500.50;
+
+        double totalbayar = hargabarang + pajak;
+        System.out.println("Jumlah Total Yang Harus di Bayar: " + totalbayar);
+    }
+}
