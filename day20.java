d891e0bd10145e3a4d3b68cbdbb05cac0119d29a
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-09-27 14:28:27 +0800

    day20.java

---
+++ day20.java
@@ -0,0 +1,10 @@
+public class day20 {
+    public static void main(String[] args) {
+        int tahunmasuk = 2024;
+        int nomorurut = 8;
+
+        final String ID_KARYAWAN = " KRY-" + tahunmasuk + "-" + nomorurut;
+
+        System.out.println("ID karyawan yang berhasil dibuat: " +ID_KARYAWAN);
+    }
+}
