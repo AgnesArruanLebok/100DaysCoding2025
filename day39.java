74d1c82f757346c6fa425d8f05d7045aa82200d2
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-10-16 20:20:15 +0800

    day39.java

--- day39.java
+++ day39.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 public class day39 {
     public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("=== MENU MINUMAN ===");
-                System.out.println("1. Es Teh     - Rp 5.000");
-                System.out.println("2. Es Jeruk   - Rp 7.000");
-                System.out.println("3. Kopi Susu  - Rp 8.000");
-                System.out.print("Masukkan pilihan (1-3): ");
-                int pilihan = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== MENU MAKANAN CEPAT SAJI ===");
+        System.out.println("1. Mie goreng      - Rp 15.000");
+        System.out.println("2. Ayam Goreng     - Rp 18.000");
+        System.out.println("3. Kentang Goreng  - Rp 10.000");
+        System.out.print("Masukkan pilihan (1-3): ");
+        int pilihan = sc.nextInt();
 
-                String pesan;
-                if (pilihan == 1) {
-                    pesan = "Anda memesan Es Teh dengan harga Rp 5.000";
-                } else if (pilihan == 2) {
-                    pesan = "Anda memesan Es Jeruk dengan harga Rp 7.000";
-                } else if (pilihan == 3) {
-                    pesan = "Anda memesan Kopi Susu dengan harga Rp 8.000";
-                } else {
-                    pesan = "Pilihan tidak valid.";
-                }
-                System.out.println(pesan);
-            }
+        String pesan;
+        if (pilihan == 1) {
+            pesan = "Anda memesan Mie goreng dengan harga Rp 15.000";
+        } else if (pilihan == 2) {
+            pesan = "Anda memesan Ayam Goreng dengan harga Rp 18.000";
+        } else if (pilihan == 3) {
+            pesan = "Anda memesan Kentang Goreng dengan harga Rp 10.000";
+        } else {
+            pesan = "Pilihan tidak valid.";
         }
-
-
-
+        System.out.println(pesan);
+    }
+}
