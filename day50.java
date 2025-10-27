a2abbdecf1761a7968f3a1aed9abff9055abd604
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-10-27 18:34:57 +0800

    day50.java

---
+++ day50.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class day50 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println(angka + " adalah bilangan " + hasil);
+
+    }
+}
