d3f708c416c9e2c10135534737e994425a6f3984
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-11-09 20:02:06 +0800

    day63.java

---
+++ day63.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class day63 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan batas angka(N): ");
+        int n = sc.nextInt();
+        int total = 1;
+        System.out.print("Proses: ");
+        for (int i = 1; i <= n; i++) {
+            total *= i;
+            System.out.print(i);
+            if (i < n) {
+                System.out.print(" x ");
+
+    }
+}
+        System.out.println(" = " + total);
+        System.out.println("\nTotal Perkalian dari 1 sampai " + n + " adalah: " + total);
+    }
+}
