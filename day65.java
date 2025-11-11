69644eca48c76cd089575ff3bd52b9b4f64e0377
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-11-11 20:06:53 +0800

    day65.java

---
+++ day65.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day65 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Program Hitung Faktorial (N!) ---");
+        System.out.print("Masukkan angka(N):");
+        int angka = sc.nextInt();
+        int hasil = 1;
+        for (int i = 1; i <= angka; i++) {
+            hasil = hasil * i;
+        }
+
+        System.out.println("\nHasil dari " + angka + "! adalah: " + hasil);
+    }
+}
