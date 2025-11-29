97b52601f2ada5e44e4eb8b08493323a7199dd56
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-11-29 19:02:37 +0800

    day83.java

---
+++ day83.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class day83 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] pengeluaran = new int[7];
+        int total = 0;
+
+        for (int i = 0; i < pengeluaran.length; i++) {
+            System.out.print("Masukkan pengeluaran hari ke-" + (i+1) + ": Rp ");
+            pengeluaran[i] = sc.nextInt();
+        }
+
+        System.out.println("\n--- Rincian Pengeluaran ---");
+        for (int i = 0; i < pengeluaran.length; i++) {
+            System.out.println("Hari ke-" + (i+1) + ": Rp " + pengeluaran[i]);
+            total += pengeluaran[i];   
+        }
+
+        System.out.println("\n---------------------------");
+        System.out.println("Total Pengeluaran Seminggu: Rp " + total);
+
+    }
+}
