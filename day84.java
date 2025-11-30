14be5e06880d99f62765e1805ed74758ecfc8a49
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-11-30 21:03:01 +0800

    day84.java

---
+++ day84.java
@@ -0,0 +1,37 @@
+//soal1 
+import java.util.Scanner;
+public class day84 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <=10; i++) {
+            System.out.println(i + " * " + n + " = " + (i * n));
+
+        }
+
+    }
+}
+//soal2
+import java.util.Scanner;
+public class day84soal2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        for (int i = 1; i <= m; i++) {
+            System.out.println("Iccang");
+        }
+    }
+}
+//soal3
+import java.util.Scanner;
+public class day84soal3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String teks = sc.nextLine();
+        int k = sc.nextInt();
+        for (int i = 1; i <= k; i++) {
+            System.out.println(teks);
+        }
+    }
+}
+
