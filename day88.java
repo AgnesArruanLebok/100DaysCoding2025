5e530eff52f1d0cfbdf3507991559bcdd7d9bd55
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-12-04 20:37:20 +0800

    day88.java

---
+++ day88.java
@@ -0,0 +1,10 @@
+public class day88 {
+    public static void main(String[] args) {
+        String [] makanan =  {"Nasi Goreng", "Soto Ayam", "Bakso", "Mie", "Ayam Geprek"};
+
+        System.out.println("=== Daftar Makanan ===");
+        for (String makananItem : makanan) {
+            System.out.println(makananItem);
+        }
+    }
+}
