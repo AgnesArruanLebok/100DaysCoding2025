5b6443425f3008bc025c71a23c80ae07596bc164
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-12-05 20:30:14 +0800

    day89.java

---
+++ day89.java
@@ -0,0 +1,9 @@
+public class day89 {
+    public static void main(String[] args) {
+        sapaDefault();
+    }
+
+    static void sapaDefault() {
+        System.out.println("Halo, selamat belajar Java!");
+    }
+}
