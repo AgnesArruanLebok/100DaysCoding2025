7e7e56d69d9302d1ba3ebb049c1e3c80b78b0fa5
AgnesArruanLebok <agnesarruanlebok@gmail.com>
2025-12-09 20:08:01 +0800

    day93.java

---
+++ day93.java
@@ -0,0 +1,9 @@
+public class day93 {
+    public static void main(String[] args) {
+        System.out.println( Math.ceil(5.4) );   
+        System.out.println( Math.floor(7.8) );  
+        System.out.println( Math.round(3.4) );  
+       
+      
+    }
+}
